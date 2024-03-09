1f4b33fc4643939a78a05160ba5a74b121d97b1c
Verseler <handuman.verselerkerr@gmail.com>
2024-03-09 23:18:31 +0800

    feat: test accelerometer

--- App.js
+++ App.js
@@ -1,10 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View, Text, Button } from 'react-native';
+import { useState, useEffect } from 'react';
+import { Accelerometer } from 'expo-sensors';
 
 export default function App() {
+  const [{x,y ,z}, setData] = useState({x: 0, y: 0, z: 0});
+
+  useEffect(() => {
+    const subscription = Accelerometer.addListener(setData);
+    return () => subscription.remove();
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+        <Text>x: {x}</Text>
+        <Text>y: {y}</Text>
+        <Text style={{color: 'red'}}>z: {z}</Text>
+        <Button title='slow' onPress={() => Accelerometer.setUpdateInterval(2000)} />
+        <Button title="Fast" onPress={() => Accelerometer.setUpdateInterval(50)} />
       <StatusBar style="auto" />
     </View>
   );
@@ -15,6 +28,6 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
-  },
+    justifyContent: 'center'
+  }
 });
