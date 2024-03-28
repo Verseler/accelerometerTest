e84785888198d5e1d3dd9487559293e288fda4d8
Verseler <handuman.verselerkerr@gmail.com>
2024-03-28 16:04:55 +0800

    added weak ang strong rc lowpass filtering

--- App.js
+++ App.js
@@ -1,27 +1,57 @@
 import React, { useState } from "react";
 import { Accelerometer } from "expo-sensors";
 import { View, Text } from "react-native";
+import KalmanFilter from "kalmanjs";
 
 const App = () => {
   const [motionData, setMotionData] = useState({ x: 0, y: 0, z: 0 });
-  const cutoffFrequency = 10; // Adjust based on your needs
+  const [strongFilteredMotionData, setMotion] = useState({ x: 0, y: 0, z: 0 });
 
-  const lowPassFilter = (value, previousValue) => {
-    const dt = 0.01; // Adjust dt based on your sampling time (in seconds)
-    const RC = 1 / (2 * Math.PI * cutoffFrequency);
+  const lowPassFilter = (currentValue, previousValue) => {
+    const dt = 1.0 / 50.0;
+    const RC = 0.15;
     const alpha = dt / (RC + dt);
-    return alpha * value + (1 - alpha) * previousValue; // Same logic as before
+
+    return alpha * currentValue + (1.0 - alpha) * previousValue;
   };
+
+  const lowPassFilterStrong = (currentValue, previousValue) => {
+    const dt = 1.0 / 50.0;
+    const RC = 0.35;
+    const alpha = dt / (RC + dt);
+
+    return alpha * currentValue + (1.0 - alpha) * previousValue;
+  };
+
   React.useEffect(() => {
     const subscription = Accelerometer.addListener((data) => {
       const filteredX = lowPassFilter(data.x, motionData.x);
-      const filteredY = lowPassFilter(data.y, motionData.x);
-      const filteredZ = lowPassFilter(data.z, motionData.x);
+      const filteredY = lowPassFilter(data.y, motionData.y);
+      const filteredZ = lowPassFilter(data.z, motionData.z);
+
+      const strFilteredX = lowPassFilterStrong(
+        data.x,
+        strongFilteredMotionData.x
+      );
+      const strFilteredY = lowPassFilterStrong(
+        data.y,
+        strongFilteredMotionData.y
+      );
+      const strFilteredZ = lowPassFilterStrong(
+        data.z,
+        strongFilteredMotionData.z
+      );
 
       setMotionData({
         x: filteredX,
         y: filteredY,
         z: filteredZ,
+      });
+
+      setMotion({
+        x: strFilteredX,
+        y: strFilteredY,
+        z: strFilteredZ,
       });
     });
 
@@ -33,14 +63,18 @@ const App = () => {
       style={{ height: "100%", alignItems: "center", justifyContent: "center" }}
     >
       <Text style={{ fontWeight: "bold" }}>Accelerometer</Text>
-      <Text>Full Values:</Text>
+      <Text>RC (0.15) Full Values:</Text>
       <Text>{motionData.x}</Text>
       <Text>{motionData.y}</Text>
       <Text>{motionData.z}</Text>
-      <Text>2 Decimal Values:</Text>
-      <Text>{motionData.x.toFixed(2)}</Text>
-      <Text>{motionData.y.toFixed(2)}</Text>
-      <Text>{motionData.z.toFixed(2)}</Text>
+      <Text style={{ marginTop: 20 }}>RC (0.15) weak lowpass filtering:</Text>
+      <Text>X: {motionData.x.toFixed(2)}</Text>
+      <Text>Y: {motionData.y.toFixed(2)}</Text>
+      <Text>Z: {motionData.z.toFixed(2)}</Text>
+      <Text style={{ marginTop: 20 }}>RC (0.35) strong lowpass filtering:</Text>
+      <Text>X: {strongFilteredMotionData.x.toFixed(2)}</Text>
+      <Text>Y: {strongFilteredMotionData.y.toFixed(2)}</Text>
+      <Text>Z: {strongFilteredMotionData.z.toFixed(2)}</Text>
     </View>
   );
 };
